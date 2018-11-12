e163538883c18ff263d3bd0ba83c1c6c64757c00
Niyazu <n.madin@campus.lmu.de>
2018-11-12 17:53:11 +0100

    printer function was added

--- printme.R
+++ printme.R
@@ -1,4 +1,7 @@
 name = "<your name>"
 eyecolor = "<your eyevcolor>"
 meal = "<your favorite meal>"
-print(paste0(name, " has ", eyecolor, " eyes and enjoys eating ", meal, " the most!"))
+#print(paste0(name, " has ", eyecolor, " eyes and enjoys eating ", meal, " the most!"))
+printer = function(name, eyecolor, meal){
+sprintf("%s has %s eyes and enjoys eating %s the most", name, eyecolor, meal)
+}
